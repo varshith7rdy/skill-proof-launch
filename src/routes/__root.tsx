195f873ff98faa25fefd8e90916d9d88e17f2ac5
Varshith Reddy Singi <varshithreddy.singi@gmail.com>
2026-09-22 10:42:16 +0530

    chore: remove favicon and all Lovable references

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,7 +89,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
